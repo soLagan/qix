6979be4a44ca1cba487a3575b652b048c7509704
Viktor Korolyuk <8334048+viktorKorolyuk@users.noreply.github.com>
2021-04-13 21:16:53 -0400

    Add fix for leftwards left-to-right incursion

--- main.py
+++ main.py
@@ -293,7 +293,8 @@ def handleCrossEdgeIncursion(touchingEdge, startingIncurringEdge, edge, board):
         or startingEdgeDirection == DIRECTION_RIGHTWARDS    and touchingEdgeDirection == DIRECTION_UPWARDS      and rightwardIncursion\
         or startingEdgeDirection == DIRECTION_RIGHTWARDS    and touchingEdgeDirection == DIRECTION_DOWNWARDS    and rightwardIncursion\
         or startingEdgeDirection == DIRECTION_LEFTWARDS     and touchingEdgeDirection == DIRECTION_UPWARDS      and leftwardIncursion\
-        or startingEdgeDirection == DIRECTION_UPWARDS       and touchingEdgeDirection == DIRECTION_DOWNWARDS    and rightwardIncursion:
+        or startingEdgeDirection == DIRECTION_UPWARDS       and touchingEdgeDirection == DIRECTION_DOWNWARDS    and rightwardIncursion\
+        or startingEdgeDirection == DIRECTION_LEFTWARDS     and touchingEdgeDirection == DIRECTION_RIGHTWARDS   and leftwardIncursion:
         startingIncurringEdge.end = board.firstEdgeBuffer.start
         startingIncurringEdge.next = board.firstEdgeBuffer
         touchingEdge.start = edge.end
