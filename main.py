0383d4f3e815f9fc889a2577d73a82e1c06d2607
Viktor Korolyuk <8334048+viktorKorolyuk@users.noreply.github.com>
2021-04-13 20:41:06 -0400

    Add fix rightwards right-to-down incursion

--- main.py
+++ main.py
@@ -290,7 +290,8 @@ def handleCrossEdgeIncursion(touchingEdge, startingIncurringEdge, edge, board):
         or startingEdgeDirection == DIRECTION_UPWARDS       and touchingEdgeDirection == DIRECTION_UPWARDS      and rightwardIncursion \
         or startingEdgeDirection == DIRECTION_UPWARDS       and touchingEdgeDirection == DIRECTION_RIGHTWARDS   and rightwardIncursion \
         or startingEdgeDirection == DIRECTION_RIGHTWARDS    and touchingEdgeDirection == DIRECTION_RIGHTWARDS   and rightwardIncursion \
-        or startingEdgeDirection == DIRECTION_RIGHTWARDS    and touchingEdgeDirection == DIRECTION_UPWARDS      and rightwardIncursion:
+        or startingEdgeDirection == DIRECTION_RIGHTWARDS    and touchingEdgeDirection == DIRECTION_UPWARDS      and rightwardIncursion\
+        or startingEdgeDirection == DIRECTION_RIGHTWARDS    and touchingEdgeDirection == DIRECTION_DOWNWARDS    and rightwardIncursion:
         startingIncurringEdge.end = board.firstEdgeBuffer.start
         startingIncurringEdge.next = board.firstEdgeBuffer
         touchingEdge.start = edge.end
